9a513c6ca3aa2cfff49c10babd882df89ef81f71
Shenfq <shenfq95@foxmail.com>
2018-03-03 17:14:58 +0800

    webpack打包后的文件添加注释

--- webpack/1-simple_load_module/dist/app.js
+++ webpack/1-simple_load_module/dist/app.js
@@ -1,125 +1,112 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/main.js");
-/******/ })
-/************************************************************************/
-/******/ ({
+(function(modules) {
+	// 已加载模块的缓存
+	var installedModules = {};
 
-/***/ "./src/main.js":
-/*!*********************!*\
-  !*** ./src/main.js ***!
-  \*********************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+	// require方法的实现
+	function __webpack_require__(moduleId) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _module_a_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./module-a.js */ "./src/module-a.js");
-/* harmony import */ var _module_b_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./module-b.js */ "./src/module-b.js");
+		// 检查该模块是否已在缓存中
+		if(installedModules[moduleId]) {
+			return installedModules[moduleId].exports;
+		}
+		// 创建一个新模块 (并将模块放入缓存对象中)
+		var module = installedModules[moduleId] = {
+			i: moduleId,
+			l: false,
+			exports: {}
+		};
+
+		// 执行模块方法
+		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+		// 标记模块为已加载
+		module.l = true;
+
+		// 返回加载后的模块对象
+		return module.exports;
+	}
 
 
+	// 暴露所有模块对象 (__webpack_modules__)
+	__webpack_require__.m = modules;
 
+	// 暴露已缓存的模块
+	__webpack_require__.c = installedModules;
 
-const people = Object.assign({}, _module_a_js__WEBPACK_IMPORTED_MODULE_0__["default"], _module_b_js__WEBPACK_IMPORTED_MODULE_1__["default"]);
+	// define getter function for harmony exports
+	__webpack_require__.d = function(exports, name, getter) {
+		if(!__webpack_require__.o(exports, name)) {
+			Object.defineProperty(exports, name, {
+				configurable: false,
+				enumerable: true,
+				get: getter
+			});
+		}
+	};
 
-console.log(people);
+	// define __esModule on exports
+	__webpack_require__.r = function(exports) {
+		Object.defineProperty(exports, '__esModule', { value: true });
+	};
 
-/***/ }),
+	// getDefaultExport function for compatibility with non-harmony modules
+	__webpack_require__.n = function(module) {
+		var getter = module && module.__esModule ?
+			function getDefault() { return module['default']; } :
+			function getModuleExports() { return module; };
+		__webpack_require__.d(getter, 'a', getter);
+		return getter;
+	};
 
-/***/ "./src/module-a.js":
-/*!*************************!*\
-  !*** ./src/module-a.js ***!
-  \*************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+	// Object.prototype.hasOwnProperty.call
+	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ({
-    name: "shenfq"
+	// __webpack_public_path__
+	__webpack_require__.p = "";
+	
+	// 加载入口模块
+	return __webpack_require__(__webpack_require__.s = "./src/main.js");
+})({
+  "./src/main.js":
+  /*!*********************!*\
+    !*** ./src/main.js ***!
+    \*********************/
+  (function(module, __webpack_exports__, __webpack_require__) {
+
+    "use strict";
+    __webpack_require__.r(__webpack_exports__);
+    var _module_a_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./module-a.js */ "./src/module-a.js");
+    var _module_b_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./module-b.js */ "./src/module-b.js");
+    const people = Object.assign({}, _module_a_js__WEBPACK_IMPORTED_MODULE_0__["default"], _module_b_js__WEBPACK_IMPORTED_MODULE_1__["default"]);
+
+    console.log(people);
+
+  }),
+  "./src/module-a.js":
+  /*!*************************!*\
+    !*** ./src/module-a.js ***!
+    \*************************/
+  (function(module, __webpack_exports__, __webpack_require__) {
+
+    "use strict";
+    __webpack_require__.r(__webpack_exports__);
+    __webpack_exports__["default"] = ({
+        name: "shenfq"
+    });
+
+  }),
+  "./src/module-b.js":
+  /*!*************************!*\
+    !*** ./src/module-b.js ***!
+    \*************************/
+  /*! exports provided: default */
+  (function(module, __webpack_exports__, __webpack_require__) {
+
+    "use strict";
+    __webpack_require__.r(__webpack_exports__);
+    __webpack_exports__["default"] = ({
+        age: 22
+    });
+
+  })
 });
-
-/***/ }),
-
-/***/ "./src/module-b.js":
-/*!*************************!*\
-  !*** ./src/module-b.js ***!
-  \*************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ({
-    age: 22
-});
-
-/***/ })
-
-/******/ });
